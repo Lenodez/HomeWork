f813f42a1fc633f6d8bf06765f5f36a304128ce3
Lenodez <Lenodez@users.noreply.github.com>
2020-09-15 08:30:41 +0300

    Внёс много исправлений в ДЗ

--- LearningPrograms/Sravnenie_kletok_shakhmat.py
+++ LearningPrograms/Sravnenie_kletok_shakhmat.py
@@ -1,15 +1,8 @@
 def cvet(BukvaCifra):
     bukvi = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    i = -1
-    sum = -1
-    while i < len(bukvi):
-        i += 1
-        if BukvaCifra[0] == bukvi[i]:
-            sum = sum + int(BukvaCifra[1])
-            sum += i
-            break
-    return sum % 2
-
+    x = int(bukvi.index(BukvaCifra[0]))
+    y = int(BukvaCifra[1])
+    return (x+y) % 2
 
 pervaya = cvet(input('Введите кординаты первой клетки '))
 vtoraya = cvet(input('Введите кординаты второй клетки '))
