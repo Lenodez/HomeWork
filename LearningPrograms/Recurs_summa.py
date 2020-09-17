d0741006d35899405c3cddcdc26b151e1294f680
Lenodez <Lenodez@users.noreply.github.com>
2020-09-17 09:37:07 +0300

    ДЗ

---
+++ LearningPrograms/Recurs_summa.py
@@ -0,0 +1,8 @@
+def sum_posl(end, shag):
+    if end <= 0:
+        return 0
+    sum_m_shag = sum_posl(end=end - shag, shag = shag)
+    return end + sum_m_shag
+
+
+print(sum_posl(8, 2))
--- LearningPrograms/Test.py
+++
@@ -1,5 +0,0 @@
-def print_ (*dicti):
-    for arg in dicti:
-        print(arg)
-diction = {'1': 'яблоки', '2': 'молоко', '3': 'хлеб', '4': 'груши', '5': 'йогурт', '6': 'мясо'}
-print_()
