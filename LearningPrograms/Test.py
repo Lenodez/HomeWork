f06f6be56c3bcd3e7c7fe145d7d1b089e9592d58
Lenodez <Lenodez@users.noreply.github.com>
2020-09-17 13:37:31 +0300

    Update Test.py

--- LearningPrograms/Test.py
+++ LearningPrograms/Test.py
@@ -1,5 +1,7 @@
-def sum_posl(nomer):
-    pass
-
+def sum_posl(chislo):
+    if chislo == (1 or 2):
+        return 1
+    chislo_pred = sum_posl(chislo-1)
+    return chislo + chislo_pred
 print(sum_posl(8))
 
