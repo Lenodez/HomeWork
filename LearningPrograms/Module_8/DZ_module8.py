7ac0d5eb8efc324291f345b7518c245fd6a01f16
Lenodez <Lenodez@users.noreply.github.com>
2020-09-25 13:00:07 +0300

    Выполение ДЗ

--- LearningPrograms/Module_8/DZ_module8.py
+++ LearningPrograms/Module_8/DZ_module8.py
@@ -3,6 +3,20 @@ class Fraction:
         self.chisl = a
         self.znamen = b
         self.drobnoe = a / b
-    def __add__(self, other):
-        pass
 
+    def __add__(self, other):
+        return self.drobnoe + other
+
+    def __radd__(self, other):
+        return other + self.drobnoe
+
+    def __sub__(self, other):
+        return self.drobnoe - other
+
+    def __rsub__(self, other):
+        return other - self.drobnoe
+
+
+chiselko = Fraction(a=1, b=2)
+drugoe = Fraction(a=2, b=1)
+print(chiselko + drugoe)
