0bb810fa3f59e403753d9252cc96711ed9131c4c
Lenodez <Lenodez@users.noreply.github.com>
2020-09-28 15:09:31 +0300

    ДЗ + добавление обучающих материалов в Модуль 8

--- LearningPrograms/Module_8/Probe.py
+++ LearningPrograms/Module_8/Probe.py
@@ -1,24 +1,104 @@
-class Multyplier:
+# -*- coding: utf-8 -*-
 
-    def __init__(self, factor=2):
-        self.factor = factor
+from random import randint
+# Реализуем модель человека.
+# Человек может есть, работать, играть, ходить в магазин.
+# У человека есть степень сытости, немного еды и денег.
+# Если сытость < 0 единиц, человек умирает.
+# Человеку надо прожить 365 дней.
+from termcolor import cprint
 
-    def __call__(self, *args):
-        res = []
-        for item in args:
-            res.append(item * self.factor)
-        return res
+
+class Man:
+
+    def __init__(self, name):
+        self.name = name
+        self.fullness = 50
+        self.house = None
+
+    def __str__(self):
+        return 'Я - {}, сытость {}'.format(
+            self.name, self.fullness)
+
+    def eat(self):
+        if self.house.food >= 10:
+            cprint('{} поел'.format(self.name), color='yellow')
+            self.fullness += 10
+            self.house.food -= 10
+        else:
+            cprint('{} нет еды'.format(self.name), color='red')
+
+    def work(self):
+        cprint('{} сходил на работу'.format(self.name), color='blue')
+        self.house.money += 50
+        self.fullness -= 10
+
+    def watch_MTV(self):
+        cprint('{} смотрел MTV целый день'.format(self.name), color='green')
+        self.fullness -= 10
+
+    def shopping(self):
+        if self.house.money >= 50:
+            cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
+            self.house.money -= 50
+            self.house.food += 50
+        else:
+            cprint('{} деньги кончились!'.format(self.name), color='red')
+
+    def go_to_the_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        cprint('{} Вьехал в дом'.format(self.name), color='cyan')
+
+    def act(self):
+        if self.fullness <= 0:
+            cprint('{} умер...'.format(self.name), color='red')
+            return
+        dice = randint(1, 6)
+        if self.fullness < 20:
+            self.eat()
+        elif self.house.food < 10:
+            self.shopping()
+        elif self.house.money < 50:
+            self.work()
+        elif dice == 1:
+            self.work()
+        elif dice == 2:
+            self.eat()
+        else:
+            self.watch_MTV()
 
 
-mul_by_27 = Multyplier(factor=27)
-result = mul_by_27(1, 2, 3, 4)
-print(result)
+class House:
 
-# multipiers = []
-# for factor in (2, 3, 4, 5):
-#     mul = Multyplier(factor=factor)
-#     multipiers.append(mul)
-# print(multipiers)
-#
-# for mul in multipiers:
-#     print(mul(10, 20, 30))
+    def __init__(self):
+        self.food = 50
+        self.money = 0
+
+    def __str__(self):
+        return 'В доме еды осталось {}, денег осталось {}'.format(
+            self.food, self.money)
+
+
+citizens = [
+    Man(name='Бивис'),
+    Man(name='Батхед'),
+    Man(name='Кенни'),
+    Man(name='Санс'),
+    Man(name='Ashen_One'),
+    Man(name='Каспер'),
+    Man(name='Толик')
+]
+
+my_sweet_home = House()
+for citisen in citizens:
+    citisen.go_to_the_house(house=my_sweet_home)
+
+for day in range(1, 10):
+    print('================ день {} =================='.format(day))
+    for citisen in citizens:
+        citisen.act()
+    print('--- в конце дня ---')
+    for citisen in citizens:
+        print(citisen)
+    print(my_sweet_home)
