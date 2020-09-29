7fb22b10ff739c42cf0ab7e88e35d2865bf0dc1a
Lenodez <Lenodez@users.noreply.github.com>
2020-09-29 16:51:38 +0300

    Эмммм

--- LearningPrograms/Module_8/Probe.py
+++ LearningPrograms/Module_8/Probe.py
@@ -1,104 +1,30 @@
-# -*- coding: utf-8 -*-
+class GrandParent:
 
-from random import randint
-# Реализуем модель человека.
-# Человек может есть, работать, играть, ходить в магазин.
-# У человека есть степень сытости, немного еды и денег.
-# Если сытость < 0 единиц, человек умирает.
-# Человеку надо прожить 365 дней.
-from termcolor import cprint
+    def method(self):
+        print('call from GrandParent')
 
 
-class Man:
+class ParentOne(GrandParent):
 
-    def __init__(self, name):
-        self.name = name
-        self.fullness = 50
-        self.house = None
-
-    def __str__(self):
-        return 'Я - {}, сытость {}'.format(
-            self.name, self.fullness)
-
-    def eat(self):
-        if self.house.food >= 10:
-            cprint('{} поел'.format(self.name), color='yellow')
-            self.fullness += 10
-            self.house.food -= 10
-        else:
-            cprint('{} нет еды'.format(self.name), color='red')
-
-    def work(self):
-        cprint('{} сходил на работу'.format(self.name), color='blue')
-        self.house.money += 50
-        self.fullness -= 10
-
-    def watch_MTV(self):
-        cprint('{} смотрел MTV целый день'.format(self.name), color='green')
-        self.fullness -= 10
-
-    def shopping(self):
-        if self.house.money >= 50:
-            cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
-            self.house.money -= 50
-            self.house.food += 50
-        else:
-            cprint('{} деньги кончились!'.format(self.name), color='red')
-
-    def go_to_the_house(self, house):
-        self.house = house
-        self.fullness -= 10
-        cprint('{} Вьехал в дом'.format(self.name), color='cyan')
-
-    def act(self):
-        if self.fullness <= 0:
-            cprint('{} умер...'.format(self.name), color='red')
-            return
-        dice = randint(1, 6)
-        if self.fullness < 20:
-            self.eat()
-        elif self.house.food < 10:
-            self.shopping()
-        elif self.house.money < 50:
-            self.work()
-        elif dice == 1:
-            self.work()
-        elif dice == 2:
-            self.eat()
-        else:
-            self.watch_MTV()
+    def method(self):
+        super().method()
+        print('call from ParentOne')
 
 
-class House:
+class ParentTwo(GrandParent):
 
-    def __init__(self):
-        self.food = 50
-        self.money = 0
-
-    def __str__(self):
-        return 'В доме еды осталось {}, денег осталось {}'.format(
-            self.food, self.money)
+    def method(self):
+        super().method()
+        print('call from ParentTwo')
 
 
-citizens = [
-    Man(name='Бивис'),
-    Man(name='Батхед'),
-    Man(name='Кенни'),
-    Man(name='Санс'),
-    Man(name='Ashen_One'),
-    Man(name='Каспер'),
-    Man(name='Толик')
-]
+class Child(ParentOne, ParentTwo, ):
 
-my_sweet_home = House()
-for citisen in citizens:
-    citisen.go_to_the_house(house=my_sweet_home)
+    def method(self):
+        super().method()
+        print('call from Child')
 
-for day in range(1, 10):
-    print('================ день {} =================='.format(day))
-    for citisen in citizens:
-        citisen.act()
-    print('--- в конце дня ---')
-    for citisen in citizens:
-        print(citisen)
-    print(my_sweet_home)
+
+obj = Child()
+obj.method()
+# print(obj.__class__.__mro__)  # так можно посмотреть в каком порядке будут искаться методы
